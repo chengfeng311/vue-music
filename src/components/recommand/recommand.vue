dc199c754cc6502f03e992c0b76f5999d4baec40
mac <v_cfcheng@tencent.com>
2019-03-14 17:25:42 +0800

    添加fastclick

--- src/components/recommand/recommand.vue
+++ src/components/recommand/recommand.vue
@@ -11,7 +11,7 @@
 			<ul>
 				<li class="item" v-for="(item, index) in discList" :key="index">
 					<div class="icon">
-						<img width="60" height="60" :src="item.imgurl" alt="">
+						<img width="60" height="60" v-lazy="item.imgurl" alt="">
 					</div>
 					<div class="text">
 						<h2 class="name">{{item.creator.name}}</h2>
@@ -90,6 +90,7 @@ export default {
 			color $color-theme
 		.item
 			display flex
+			height 70px
 			justify-content space-around
 			.icon
 				flex 1
--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import 'common/stylus/index.styl'
 import 'babel-polyfill'
 import 'swiper/dist/css/swiper.css'
+import VueLazyload from 'vue-lazyload'
+import FastClick from 'fastclick'
 import Vue from 'vue'
 import App from './App'
 import router from './router/index'
 
 Vue.config.productionTip = false
 
+FastClick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'common/image/default.png',
+  loading: 'common/image/default.png',
+  attempt: 1,
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
